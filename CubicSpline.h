69a3c318fae632e7e891daff4b109b298cb2eaab
gorartano <alex.undr.a17@gmail.com>
2023-10-23 02:12:20 +0300

    Cubic Spline lab add

---
+++ CubicSpline.h
@@ -0,0 +1,70 @@
+﻿// Spline.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+class CubicSpline {
+private:
+    std::vector <double> X, a, b, c, d, m, l, z, alpha;
+    double h;
+    int n;
+
+
+public:
+
+
+    explicit CubicSpline(const std::vector<double>& xArr, const std::vector<double>& yArr) : n(yArr.size() - 1), h(xArr[1] - xArr[0])
+    {
+        a.insert(a.begin(), yArr.begin(), yArr.end());
+
+        X.insert(X.begin(), xArr.begin(), xArr.end());
+        b.resize(n);
+        d.resize(n);
+        c.resize(n + 1);
+        l.resize(n + 1);
+        m.resize(n + 1);
+        z.resize(n + 1);
+
+        l[0] = 1.0;
+        z[0] = 0.0;
+        m[0] = 0.0;
+        alpha.push_back(0.0);
+
+        for (int i = 1; i < n; i++)
+            alpha.push_back((3.0 * (a[i + 1] - a[i]) - 3.0 * (a[i] - a[i - 1])) / h); //n-1 ýëåìåíòîâ
+
+        for (int i = 1; i < n; i++) {
+            l[i] = 2 * (xArr[i + 1] - xArr[i - 1]) - h * m[i - 1];
+            m[i] = h / l[i];
+            z[i] = (alpha[i] - h * z[i - 1]) / l[i];
+        }
+
+        l[n] = 1.0;
+        z[n] = 0.0;
+        c[n] = 0.0;
+
+        for (int i = n - 1; i > 0; i--) {
+            c[i] = z[i] - m[i] * c[i + 1];
+            b[i] = (a[i + 1] - a[i]) / h - h * (c[i + 1] + 2.0 * c[i]) / 3.0;
+            d[i] = (c[i + 1] - c[i]) / (3.0 * h);
+        }
+    }
+
+
+    [[nodiscard]] double interpolate(double x) const
+    {
+        if (x == X[n]) return a[n];
+        else
+        {
+            const unsigned int i = x / h;
+            const double b_coeff = x - X[i];
+            const double c_coeff = b_coeff * b_coeff;
+            const double d_coeff = c_coeff * b_coeff;
+            return a[i] + b[i] * b_coeff + c[i] * c_coeff + d[i] * d_coeff;
+        }
+    }
+};
---
+++ Spline.cpp
@@ -0,0 +1,47 @@
+﻿// Spline.cpp : Defines the entry point for the application.
+//
+
+#include "CubicSpline.h"
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+
+
+int main()
+{
+    std::vector <double> error;
+    double l = 10;
+
+
+    std::ofstream outfile("output_spline.txt");
+    for (int j = 0; j < 6; j++) {
+        int step = 5;
+        int N = step * pow(2, j);
+        std::vector <double> x, y;
+        for (int i = 0; i < N; i++) {
+            x.push_back(i * l / (N - 1));
+            y.push_back(exp(x[i]));
+        }
+
+        CubicSpline A(x, y);
+
+        double delta_max = 0;
+        for (int k = 0; k < 1000; k++) {
+            double theory = exp(k * l / 999);
+            double real = A.interpolate(k * l / 999);
+            double delta = theory - real;
+            if (delta < 0) delta = delta * (-1);
+            if (delta > delta_max) delta_max = delta;
+        }
+        error.push_back(delta_max);
+        std::cout << N << ' ' << delta_max << std::endl;
+        outfile << N << " ";
+        outfile << delta_max;
+        outfile << std::endl;
+
+    }
+    outfile.close();
+}
+
