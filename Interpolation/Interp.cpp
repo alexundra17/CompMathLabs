b04c70177a322855fe27ef732c5d53957551262f
gorartano <alex.undr.a17@gmail.com>
2023-10-23 04:10:24 +0300

    Interp lab added

---
+++ Interpolation/Interp.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <fstream>
+#include"interp.h"
+
+int main()
+{
+    const int j_max = 6;
+    std::vector <double> ox, error, length;
+    double step = 0.0625;
+    for (int j = 0; j < j_max; j++) {
+        std::vector <double> x, vect;
+        double l = pow(2, j) * step;
+        ox.push_back(l);
+
+        int N = 4;
+        //равномерное распределение узлов
+    //    for (int i = 0; i < N; i++) {
+    //        x.push_back(i * l / (N - 1));
+    //        vect.push_back(exp(x[i]));
+    //    }
+
+    //    NewtonianInterpolator A(x, vect);
+
+    //    double delta_max = 0;
+    //    for (int k = 0; k < 1000; k++) {
+    //        double theory = exp(k * l / 999);
+    //        double real = A.interpolate(k * l / 999);
+    //        double delta = theory - real;
+    //        if (delta < 0) delta = delta * (-1);
+    //        if (delta > delta_max) delta_max = delta;
+    //    }
+    //    error.push_back(delta_max);
+    //    length.push_back(l);
+
+    //}
+    //std::ofstream outfile("output_interp_uni_5.txt");
+    //for (int i = 0; i < j_max; i++) {
+    //    std::cout << length[i] << ' ' << error[i] << std::endl;
+    //    outfile << length[i] << " ";
+    //    outfile << error[i];
+    //    outfile << std::endl;
+
+    //}
+    //outfile.close();
+  //Чебышевское распределение узлов
+        for (int i = 0; i < N; i++) {
+            double pi = std::acos(-1);
+            x.push_back(l / 2 + l / 2 * std::cos(pi * (2 * i + 1) / (2 * N + 2)));
+            vect.push_back(exp(x[i]));
+        }
+
+        NewtonianInterpolator A(x, vect);
+
+        double delta_max = 0;
+        for (int k = 0; k < 1000; k++) {
+            double theory = exp(k * l / 999);
+            double real = A.interpolate(k * l / 999);
+            double delta = theory - real;
+            if (delta < 0) delta = delta * (-1);
+            if (delta > delta_max) delta_max = delta;
+        }
+        error.push_back(delta_max);
+        length.push_back(l);
+
+    }
+    std::ofstream outfile("output_interp_ch_4.txt");
+    for (int i = 0; i < j_max; i++) {
+        std::cout << length[i] << ' ' << error[i] << std::endl;
+        outfile << length[i] << " ";
+        outfile << error[i];
+        outfile << std::endl;
+
+    }
+    outfile.close();
+}
---
+++ Interpolation/Interp.h
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <fstream>
+
+class NewtonianInterpolator {
+private:
+    double** matrix;
+    int size;
+
+public:
+    explicit NewtonianInterpolator(const std::vector<double>& x, const std::vector<double>& y) : size(int(x.size())) {
+        matrix = new double* [size];
+        for (int i = 0; i < size; i++) {
+            matrix[i] = new double[i + 2];
+            matrix[i][0] = x[i];
+            matrix[i][1] = y[i];
+        }
+        for (int i = 2; i < size + 2; i++) {
+            for (int j = i - 1; j < size; j++) {
+                double p = (matrix[j][i - 1] - matrix[j - 1][i - 1]);
+                double q = (matrix[j][0] - matrix[j - i + 1][0]);
+                matrix[j][i] = p / q;
+            }
+        }
+    }
+    [[nodiscard]] double interpolate(double x) const {
+        double func = matrix[size - 1][size];
+        for (int i = 1; i < size; i++) {
+            double p = matrix[size - i - 1][size - i];
+            func = func * (x - matrix[size - i - 1][0]) + p;
+        }
+        return func;
+    }
+    ~NewtonianInterpolator() {
+        for (unsigned int i = 0; i < size; i++) delete matrix[i];
+        delete[]matrix;
+    }
+};
